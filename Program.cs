2fc3c7690c9b416cbd127ff80775beb6d46f1551
Kieran David Evans <keyz182@gmail.com>
2021-02-28 14:44:45 +0000

    More control options, and properly clear LEDs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Drawing;
 using System.Threading.Tasks;
-using rpi_ws281x;
 using DuetAPI.Connection;
-using DuetAPIClient;
 
 namespace LEDProgress
 {
@@ -14,8 +11,10 @@ namespace LEDProgress
             // Parse the command line arguments
             int pin = 12;
             int count = 56;
+            byte brightness = 255;
             string lastArg = null, socketPath = Defaults.FullSocketPath;
             bool quiet = false;
+            bool invert = false;
             foreach (string arg in args)
             {
                 if (lastArg == "-s" || lastArg == "--socket")
@@ -34,19 +33,29 @@ namespace LEDProgress
                 {
                     quiet = true;
                 }
+                else if (arg == "-i" || arg == "--invert")
+                {
+                    invert = true;
+                }
+                else if (arg == "-b" || arg == "--brightness")
+                {
+                    brightness = byte.Parse(arg);
+                }
                 else if (arg == "-h" || arg == "--help")
                 {
                     Console.WriteLine("Available command line arguments:");
                     Console.WriteLine("-s, --socket <socket>: UNIX socket to connect to");
                     Console.WriteLine("-p, --pin <pin>: The pin the LEDs are run from");
+                    Console.WriteLine("-i, --invert: WS281x Invert");
                     Console.WriteLine("-c, --count <count>: The number of LEDs");
+                    Console.WriteLine("-b, --brightness <brightness>: Max brighness (0-255)");
                     Console.WriteLine("-h, --help: Display this help text");
                     return;
                 }
                 lastArg = arg;
             }
 
-            Subscriber sub = new Subscriber(pin, socketPath, count);
+            Subscriber sub = new Subscriber(pin, socketPath, count, quiet, brightness, invert);
 
             await sub.Subscribe();
         }
--- Subscriber.cs
+++ Subscriber.cs
@@ -13,34 +13,48 @@ namespace LEDProgress
 {
     class Subscriber{
         private string _socketPath;
-        private int _pin;
         private int _count;
+        private byte _brightness;
+        private bool _quiet = false;
 
         private Settings _settings;
 
-        public Subscriber(int pin, string socketPath, int count){
-            this._pin = pin;
+        private void Log(string str){
+            if(this._quiet) return;
+            Console.WriteLine(str);
+        }
+
+        public Subscriber(int pin, string socketPath, int count, bool quiet, byte brightness, bool invert){
             this._socketPath = socketPath;
             this._count = count;
+            this._quiet = quiet;
+            this._brightness = brightness;
             //The default settings uses a frequency of 800000 Hz and the DMA channel 10.
 			this._settings = Settings.CreateDefaultSettings();
-  
+
+            Log("Initializing WS281x");
 			//Set brightness to maximum (255)
 			//Use Unknown as strip type. Then the type will be set in the native assembly.
-			this._settings.Channel_1 = new Channel(this._count, pin, 255, false, StripType.WS2812_STRIP);
+			this._settings.Channel_1 = new Channel(this._count, pin, this._brightness, invert, StripType.WS2812_STRIP);
         }
 
         public async Task Subscribe(CancellationToken cancellationToken = default(CancellationToken)){
             // Create a new connection and connect to DuetControlServer
             // https://duet3d.github.io/DuetSoftwareFramework/api/DuetAPIClient.SubscribeConnection.html
+            Log("Connecting to Duet");
             using (var controller = new WS281x(this._settings))
             using (SubscribeConnection connection = new SubscribeConnection()){
                 List<string> filter = null;
                 await connection.Connect(SubscriptionMode.Full, filter, this._socketPath, cancellationToken);
 
+                Log("Fetching Object Model");
                 ObjectModel model = await connection.GetObjectModel(cancellationToken);
 
                 do {
+                    for(var i = 0; i < this._count; i++){
+				        controller.SetLEDColor(0, i, Color.Black);
+                    }
+
                     int duration = model.Job.Duration ?? 0;
                     int left = model.Job.TimesLeft.File ?? 0;
 
@@ -54,22 +68,33 @@ namespace LEDProgress
                     for(var i = 0; i < fullLeds; i++){
 				        controller.SetLEDColor(0, i, Color.Green);
                     }
-                    var hsv = (new Hex("00FF00")).To<Hsv>();
-                    hsv.S = partial;
-                    var color = Color.FromArgb((int)hsv.ToRgb().R, (int)hsv.ToRgb().G, (int)hsv.ToRgb().B);
-                    controller.SetLEDColor(0, fullLeds, color);
 
+                    controller.SetLEDColor(0, fullLeds, GetPartialColor(partial));
                     controller.Render();
 
-                    Console.WriteLine(percentage);
-				    System.Threading.Thread.Sleep(1000 / 15);
+                    Log(string.Format("{0}%", percentage));
+
+				    System.Threading.Thread.Sleep(25);
 
                     //TODO LEDS HERE
+                    Log("Updating Model");
                     var json = await connection.GetObjectModelPatch(cancellationToken);
                     model.UpdateFromJson(json.RootElement);
                 }while(!cancellationToken.IsCancellationRequested);
-                
+            
+                for(var i = 0; i < this._count; i++){
+                    controller.SetLEDColor(0, i, Color.Black);
+                }
+
+                controller.Render();
             }
+        }
+
+        public Color GetPartialColor(double partial){
+            var hsv = (new Hex("00FF00")).To<Hsv>();
+            hsv.S = partial;
+            hsv.V = partial;
+            return Color.FromArgb((int)hsv.ToRgb().R, (int)hsv.ToRgb().G, (int)hsv.ToRgb().B);
         }
     }
 }
